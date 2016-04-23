01ade21f9a6a497333d8aa7227b8f268c4683a1d
PedroJoaoPereira <pedro.pereira.95@hotmail.com>
2016-04-23 16:35:18 +0100

    Para o Romano

--- SERVER/Proj/src/entry/MainClass.java
+++ SERVER/Proj/src/entry/MainClass.java
@@ -1,8 +1,17 @@
 package entry;
 
+import java.sql.*;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:db/data.db");
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
 	}
 }
