32f7c3563a7a4fb9be996f2384b1355fc5795096
Tropical Drink <emailfalsonumero4@gmail.com>
2016-04-23 16:16:50 +0100

    :tangerine:

--- PC/index.php
+++ PC/index.php
@@ -2,12 +2,21 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Web</title>
+	<script src="JQuery/jquery-1.12.3.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="Styles/index.css">
+	<title>index page</title>
 	
 </head>
 <body>
-	<h1></h1>
-
+	<header>
+		<div id="login">
+			<input type="text" name="username_login"></input>
+			<input type="password" name="password_login"></input>
+		</div>
+	</header>
+	
+	<div>
+	</div>
 	
 </body>
 </html>
